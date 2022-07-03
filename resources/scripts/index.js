e0113c1b61055ed592d93010ef3b7a37e92b0140
Ferit Yiğit BALABAN <fyb@duck.com>
2022-07-03 22:33:27 +0300

    Animated brands section

---
+++ resources/scripts/index.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains('reverse'))
+                entry.target.classList.add('table-item-animation-reverse');
+            else
+                entry.target.classList.add('table-item-animation');
+        }
+    });
+});
+
+let targets = document.querySelectorAll('.table-item');
+targets.forEach(target => {
+    observer.observe(target);
+})
