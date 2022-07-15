1a8ee91e3e99a8b7292b36faf410b8372ef97e92
Ferit Yiğit BALABAN <fyb@duck.com>
2022-07-15 22:35:14 +0300

    AAAAAyes

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,3 +1,23 @@
+const doSlideshow = true;
+const slideTime = 7500; // milliseconds
+const animTime = 1000; // milliseconds
+
+async function slideshow() {
+    let showcase = document.getElementsByClassName("showcase")[0];
+    while(doSlideshow) {
+        showcase.style.backgroundImage = "url(resources/img/index/g.jpg)";
+        showcase.classList.add("showcase-animation");
+        await sleep(animTime);
+        showcase.classList.remove("showcase-animation");
+        await sleep(slideTime);
+        showcase.style.backgroundImage = "url(resources/img/index/y.jpg)";
+        showcase.classList.add("showcase-animation");
+        await sleep(animTime);
+        showcase.classList.remove("showcase-animation");
+        await sleep(slideTime);
+    }
+}
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -9,9 +29,9 @@ const observer = new IntersectionObserver(entries => {
     });
 });
 
-
-
 let targets = document.querySelectorAll('.table-item');
 targets.forEach(target => {
     observer.observe(target);
 })
+
+slideshow();
