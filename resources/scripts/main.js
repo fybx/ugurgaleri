6d3269395a3287135175c0a360a33106d017c443
Ferit Yiğit BALABAN <fyb@duck.com>
2022-07-28 11:16:39 +0300

    Add transition to drawer-item

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -2,6 +2,7 @@ const SCROLL_MIN = 100;
 scroll_to_top = document.getElementById("scroll-to-top");
 el_drawer = document.getElementById("drawer");
 el_container = document.getElementById("container");
+$drawer_item = document.querySelectorAll(".drawer-item");
 
 function checkScroll() {
     if (document.documentElement.scrollTop > SCROLL_MIN || document.body.scrollTop > SCROLL_MIN) {
@@ -20,20 +21,18 @@ async function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-let navitems = document.getElementsByClassName('dropdown-nav-item');
-
 async function toggleDropdown() {
     let expanded = el_drawer.classList.contains("expanded");
     if (expanded) {
-        for (let i = 0; i < navitems.length; i++) {
-            navitems[i].classList.toggle("visible");
+        for (let i = 0; i < $drawer_item.length; i++) {
+            $drawer_item[i].classList.toggle("visible");
         }
         el_drawer.classList.toggle("expanded");
     } else {
         el_drawer.classList.toggle("expanded");
-        await sleep(500);
-        for (let i = 0; i < navitems.length; i++) {
-            navitems[i].classList.toggle("visible");
+        await sleep(250);
+        for (let i = 0; i < $drawer_item.length; i++) {
+            $drawer_item[i].classList.toggle("visible");
         }
     }
 }
